6edada98acd754232f88aa8b066cf2e976616fd5
hanzhi713 <hanzhi713@163.com>
2020-02-17 23:30:31 -0500

    configuration update

--- src/navigation/src/static_tf.cpp
+++ src/navigation/src/static_tf.cpp
@@ -7,23 +7,24 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "static_transforms");
     static tf2_ros::StaticTransformBroadcaster static_broadcaster;
 
-    geometry_msgs::TransformStamped static_transformStamped;
+    // base to camera transform
+    geometry_msgs::TransformStamped base_camera;
 
-    static_transformStamped.header.stamp = ros::Time::now();
-    static_transformStamped.header.frame_id = "base_link";
-    static_transformStamped.child_frame_id = "camera_link";
+    base_camera.header.stamp = ros::Time::now();
+    base_camera.header.frame_id = "base_link";
+    base_camera.child_frame_id = "camera_link";
 
-    auto& trans = static_transformStamped.transform.translation;
+    auto& trans = base_camera.transform.translation;
     trans.x = 0;
     trans.y = 0;
     trans.z = 0;
 
-    auto& rot = static_transformStamped.transform.rotation;
+    auto& rot = base_camera.transform.rotation;
     rot.x = 0;
     rot.y = 0;
     rot.z = 0;
     rot.w = 1;
-    static_broadcaster.sendTransform(static_transformStamped);
+    static_broadcaster.sendTransform(base_camera);
 
     ROS_INFO("Spinning until killed publishing to world");
     ros::spin();
