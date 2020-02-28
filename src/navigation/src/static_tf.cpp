6c7417859217642007a9fe01c8cd48f7eddfd080
hanzhi713 <hanzhi713@163.com>
2020-02-27 20:53:41 -0500

    transforms and launch file update

--- src/navigation/src/static_tf.cpp
+++ src/navigation/src/static_tf.cpp
@@ -21,26 +21,37 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "static_transforms");
     static tf2_ros::StaticTransformBroadcaster static_broadcaster;
 
-    // base to camera transform
-    geometry_msgs::TransformStamped base_camera;
+    // base to cameras transform
+    geometry_msgs::TransformStamped base_d435_camera;
+    geometry_msgs::TransformStamped t265_base;
+
+    base_d435_camera.header.stamp = ros::Time::now();
+    base_d435_camera.header.frame_id = "base_link";
+    base_d435_camera.child_frame_id = "d435_link";
+
+    t265_base.header.stamp = ros::Time::now();
+    t265_base.header.frame_id = "t265_link";
+    t265_base.child_frame_id = "base_link";
+
+    setTranslation(base_d435_camera.transform.translation);
+    setRotation(base_d435_camera.transform.rotation);
+    static_broadcaster.sendTransform(base_d435_camera);
+
+    setTranslation(t265_base.transform.translation, 0, 0, 0.5);
+    setRotation(t265_base.transform.rotation);
+    static_broadcaster.sendTransform(t265_base);
+
+    // ----------------------- dummy transform ------------------
     // map to odom transform
     geometry_msgs::TransformStamped map_odom;
-
-    base_camera.header.stamp = ros::Time::now();
-    base_camera.header.frame_id = "base_link";
-    base_camera.child_frame_id = "camera_link";
-
     map_odom.header.stamp = ros::Time::now();
     map_odom.header.frame_id = "map";
-    map_odom.child_frame_id = "odom";
-
-    setTranslation(base_camera.transform.translation);
-    setRotation(base_camera.transform.rotation);
-    static_broadcaster.sendTransform(base_camera);
+    map_odom.child_frame_id = "t265_odom_frame";
 
     setTranslation(map_odom.transform.translation);
     setRotation(map_odom.transform.rotation);
     static_broadcaster.sendTransform(map_odom);
+    // ----------------------- dummy transform ------------------
 
     ROS_INFO("Spinning until killed publishing to world");
     ros::spin();
