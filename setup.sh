7ba2978946c70f9e3499913f54507cc81c78e3fc
ehanover <ephanover@gmail.com>
2022-05-13 13:21:35 -0400

    make setup script do hero chmod

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "Enter this device's IP address (laptop)": \(without http:// or port\)
+echo "Enter this device's IP address (laptop): (without http:// or port)"
+echo "    (if running this in a VM, enter the host computer's IP)"
 read IP
 
 source /opt/ros/melodic/setup.bash
@@ -13,6 +14,12 @@ elif [ -f "devel/setup.bash" ]; then
     source devel/setup.bash
 else
     echo "Could not find a ROS setup.bash file! ROS commands may not work."
+fi
+
+if [ -f "/dev/ttyUSB0" ]; then
+    chmod 666 /dev/ttyUSB0 # requires sudo but will prompt the user for password
+else
+    echo "Could not find device /dev/ttyUSB0 (HERO board)! File permissions may be wrong and it may not be found by ROS."
 fi
 
 
