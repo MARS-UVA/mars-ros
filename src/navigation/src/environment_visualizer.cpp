57132d662583e69819781c8735277898ceaff656
ehanover <ephanover@gmail.com>
2023-04-28 14:00:32 -0400

    add rviz marker visualization for deposit bin

--- src/navigation/src/environment_visualizer.cpp
+++ src/navigation/src/environment_visualizer.cpp
@@ -5,75 +5,129 @@
 
 
 // https://wiki.ros.org/rviz/Tutorials/Markers%3A%20Basic%20Shapes
-visualization_msgs::Marker constructMarker(const XmlRpc::XmlRpcValue& value) {
+visualization_msgs::Marker constructApriltagMarker(const XmlRpc::XmlRpcValue& value) {
 
-  visualization_msgs::Marker marker;
+  visualization_msgs::Marker apriltag_marker;
   // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-  marker.header.frame_id = "map";
-  marker.header.stamp = ros::Time::now();
+  apriltag_marker.header.frame_id = "map";
+  apriltag_marker.header.stamp = ros::Time::now();
 
   // Set the namespace and id for this marker.  This serves to create a unique ID
   // Any marker sent with the same namespace and id will overwrite the old one
-  marker.ns = "tag_positions";
-  marker.id = (int) value["id"];
+  apriltag_marker.ns = "tag_positions";
+  apriltag_marker.id = (int) value["id"];
 
   // Set the marker type.
-  marker.type = visualization_msgs::Marker::CUBE;
+  apriltag_marker.type = visualization_msgs::Marker::CUBE;
 
   // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-  marker.action = visualization_msgs::Marker::ADD;
+  apriltag_marker.action = visualization_msgs::Marker::ADD;
 
   // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-  marker.pose.position.x = (double) value["xpos"];
-  marker.pose.position.y = (double) value["ypos"];
-  marker.pose.position.z = (double) value["zpos"];
+  apriltag_marker.pose.position.x = (double) value["xpos"];
+  apriltag_marker.pose.position.y = (double) value["ypos"];
+  apriltag_marker.pose.position.z = (double) value["zpos"];
 
   tf2::Quaternion tq;
   tq.setRPY(0, 0, (double) value["yaw"]);
   geometry_msgs::Quaternion gq = tf2::toMsg(tq);
-  marker.pose.orientation = gq;
+  apriltag_marker.pose.orientation = gq;
 
+  /* April tag marker dimensions and color: */
   // Set the scale of the marker in meters
-  marker.scale.x = 0.1;
-  marker.scale.y = 0.02;
-  marker.scale.z = 0.1;
+  apriltag_marker.scale.x = 0.1;
+  apriltag_marker.scale.y = 0.02;
+  apriltag_marker.scale.z = 0.1;
 
   // Set the color -- be sure to set alpha to something non-zero!
-  marker.color.r = 0.0;
-  marker.color.g = 0.7;
-  marker.color.b = 1.0;
-  marker.color.a = 1.0;
+  apriltag_marker.color.r = 0.0;
+  apriltag_marker.color.g = 0.7;
+  apriltag_marker.color.b = 1.0;
+  apriltag_marker.color.a = 1.0;
 
-  marker.lifetime = ros::Duration();
+  apriltag_marker.lifetime = ros::Duration();
 
-  return marker;
+  return apriltag_marker;
+}
+
+visualization_msgs::Marker constructBinMarker(const XmlRpc::XmlRpcValue& value) {
+
+  visualization_msgs::Marker bin_marker;
+  bin_marker.header.frame_id = "map";
+  bin_marker.header.stamp = ros::Time::now();
+
+  bin_marker.ns = "bin_position";
+  bin_marker.id = 0;
+
+  bin_marker.type = visualization_msgs::Marker::CUBE;
+  bin_marker.action = visualization_msgs::Marker::ADD;
+
+  bin_marker.pose.position.x = (double) value["xpos"];
+  bin_marker.pose.position.y = (double) value["ypos"];
+  bin_marker.pose.position.z = (double) value["zpos"];
+
+  tf2::Quaternion tq;
+  tq.setRPY(0, 0, 0);
+  geometry_msgs::Quaternion gq = tf2::toMsg(tq);
+  bin_marker.pose.orientation = gq;
+
+  bin_marker.scale.x = 0.1;
+  bin_marker.scale.y = 0.5;
+  bin_marker.scale.z = 0.1;
+
+  bin_marker.color.r = 1.0;
+  bin_marker.color.g = 0.2;
+  bin_marker.color.b = 0.3;
+  bin_marker.color.a = 1.0;
+
+  bin_marker.lifetime = ros::Duration();
+
+  return bin_marker;
 }
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "environment_visualizer_node");
 
   ros::NodeHandle nodeHandle;
-  ros::Publisher publisher = nodeHandle.advertise<visualization_msgs::Marker>("tag_positions", 10); // queue size
+  ros::Publisher publisher = nodeHandle.advertise<visualization_msgs::Marker>("positions", 1); // queue size
 
+  XmlRpc::XmlRpcValue standalone_apriltag_positions;
+  XmlRpc::XmlRpcValue bin_position;
+  if(nodeHandle.getParam("tag_positions/standalone_tags", standalone_apriltag_positions)) {
+    ROS_INFO("Detected tag position subscriber, successfully loaded apriltag positions");
+  } else {
+    ROS_ERROR("Failed to load apriltag positions for visualization");
+    return 1;
+  }
+ 
+  if(nodeHandle.getParam("bin_position", bin_position)) {
+    ROS_INFO("Detected bin position subscriber, successfully loaded bin position");
+  } else {
+    ROS_ERROR("Failed to load bin positions for visualization");
+    return 1;
+  }
+
+
+  // wait until there's a subscriber to the topic before publishing
   while(publisher.getNumSubscribers() < 1) {
-    if(!ros::ok()) {
+    if(!nodeHandle.ok()) {
       return 0;
     }
     sleep(1);
   }
 
-  XmlRpc::XmlRpcValue standalone_positions;
-  if(nodeHandle.getParam("tag_positions/standalone_tags", standalone_positions)) {
-    ROS_INFO("Detected tag position subscriber, successfully loaded positions");
-  } else {
-    ROS_ERROR("Failed to load apriltag positions for visualization");
-    return 1;
+  // once there's a subscriber, publish periodically just for good measure (ie if someone new subscribes) even though the data isn't changing
+  ros::Rate r(0.2); // 0.2 hz = every 5 seconds
+  while (nodeHandle.ok()) {
+    for(int i=0; i<standalone_apriltag_positions.size(); i++) {
+      publisher.publish(constructApriltagMarker(standalone_apriltag_positions[i]));
+    }
+    
+    publisher.publish(constructBinMarker(bin_position));
+
+    ros::spinOnce();
+    r.sleep();
   }
 
-  for(int i=0; i<standalone_positions.size(); i++) {
-    publisher.publish(constructMarker(standalone_positions[i]));
-  }
-
-  ros::spin();
   return 0;
 };
