4491917b9edf96f00651e22421230d157917b7d8
Mikhail Kornilov <vboxuser@mars2324.myguest.virtualbox.org>
2023-11-12 16:11:34 -0500

    Fixed steps 5, 6, 8

--- start.sh
+++ start.sh
@@ -1,17 +1,18 @@
 #Instructions: sudo ./start.sh <jetson's IP address>
-#Step 5
+#Step 5 - Probably done, not sure how to test
 jetsonIP=$1
 sshpass
 if [ $? = 127 ]
 then
 	apt install sshpass
 fi
-gnome-terminal -e "bash -c \"sshpass -p nvidiauva ssh nvidia@$jetsonIP \"cd mars-ros; ./setup.sh; roslaunch navigation malvi_config.launch;\"\""
 
+sshpass -p nvidiauva ssh nvidia@$jetsonIP 'cd mars-ros; ./setup.sh; source devel_isolated/setup.bash; roslaunch navigation malvi_config.launch'
+echo "Done with SSH"
 
 #Step 6 - Tested
 cd
-ethernetAddress=`ifconfig | grep -o ..:..:..:..:..:..`
+ethernetAddress=`ifconfig | grep -o ....::....:....:....:....`
 nvidiaCount=`grep nvidia /etc/hosts -c`
 if [ $nvidiaCount -gt 0 ]
 then
@@ -22,9 +23,10 @@ fi
 echo "Ethernet IP is $ethernetAddress"
 
 
-#Step 7
-gnome-terminal -e "bash -c 'source ../mars-ros/devel_isolated/setup.bash; export ROS_MASTER_URI=http://nvidia:11311; export ROS_IP=$jetsonIP; roslaunch rosbridge_server rosbridge_websocket.launch;'"
+#Step 7 - currently broken
+gnome-terminal -e "bash -c 'source ../mars-ros/devel_isolated/actions/setup.bash; export ROS_MASTER_URI=http://nvidia:11311; export ROS_IP=$jetsonIP; roslaunch rosbridge_server rosbridge_websocket.launch;sleep 20'"
 
 
-#Step 8
-gnome-terminal -e "bash -c 'cd mars/mars-ui-web; source ../mars-ros/devel_isolated/setup.bash; npm start'"
+#Step 8 - Tested
+gnome-terminal -e "bash -c 'pwd; cd mars/mars-ros; cd ../mars-ui-web; source ../mars-ros/devel/setup.bash; npm start; sleep 20'"
+
