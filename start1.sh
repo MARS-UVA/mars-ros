9feafb1563d4ca399b40d50c25a8450c81b484ed
MARS at UVA <hoosmininguva@gmail.com>
2023-12-11 16:32:33 -0500

    Fixed bug in using startup script with Docker

--- start1.sh
+++ start1.sh
@@ -1,8 +1,18 @@
+<<instructions
+Run this script on the control station when you want to connect the control station 
+to the Jetson (the computer onboard the robot). This should be run BEFORE start2.sh.
+
+Instructions: ./start2.sh or ./start2.sh <jetson's IP address>
+instructions
+
+# Define the IP address of the Jetson
+JETSON_IP=${1:-"192.168.0.2"}
+
 # SSH into the Jetson and start the ROS nodes there
-if [ $? = 127 ]
+sshpass > /dev/null # try to run sshpass, but don't print out the text that you would normally see
+if [ $? = 127 ] # check the last error code to see if it corresponds to the command not being installed
 then
-	apt install sshpass
+	echo "mars2324" | sudo -S apt install sshpass
 fi
 
-
-# gnome-terminal -- bash -c "sshpass -p nvidiauva ssh nvidia@192.168.0.2 \"docker run -d --network host malvi-config:1.0\""
+sshpass -p nvidiauva ssh -tt nvidia@$JETSON_IP "docker run -it --network host malvi-config:1.0"
