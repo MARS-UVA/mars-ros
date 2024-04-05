8bf75971eb03d9360ea54d94ea71b82c5a6638ab
MKhour <67045178+MKhour@users.noreply.github.com>
2024-04-05 17:08:28 -0400

    fixed registry of ir processing node

--- src/actions/scripts/ir_adc_node.py
+++ src/actions/scripts/ir_adc_node.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 import rospy
+from std_msgs.msg import String
 
 pub = None
 
 def setup_node():
+    global pub
     pub = rospy.Publisher("ir-adc", String, queue_size=0)
     rospy.init_node("ir-adc-node", anonymous=True)
     
 def publish_ir():
+    global pub
     pub.publish("test")
 
 if __name__ == "__main__":
