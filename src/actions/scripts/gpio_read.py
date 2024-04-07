dbfad95450a36d729443a044a32b6ab272b6ce5c
Hao Gu <haogu2003@gmail.com>
2024-04-07 14:56:03 -0400

    added custom feeeback type and make gpio read two channels

--- src/actions/scripts/gpio_read.py
+++ src/actions/scripts/gpio_read.py
@@ -1,22 +1,28 @@
 import rospy
-from std_msgs.msg import String
 import Jetson.GPIO as GPIO
+from actions.msg import DigitalFeedbackGpio
 
 pub = None
-channel = 12 #GIOP pin number
+channel_bucket = 12 
+channel_bin = 16 
+feedback = DigitalFeedbackGpio()
 
 def setup_node():
     global pub
-    pub = rospy.Publisher("gpio", String, queue_size=0)
+    pub = rospy.Publisher("gpio", DigitalFeedbackGpio, queue_size=0)
     rospy.init_node("gpio_read", anonymous = True)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(channel, GPIO.IN)
+    GPIO.setup(channel_bucket, GPIO.IN)
+    GPIO.setup(channel_bin, GPIO.IN)
 
 def publish():
     global pub
-    gpio_state = GPIO.input(channel)
-    pub.publish(str(gpio_state))
-    rospy.loginfo("GPIO state: %s" % gpio_state)
+    gpio_state_bucket = GPIO.input(channel_bucket)
+    gpio_state_bin = GPIO.input(channel_bin)
+    feedback.bucket_spinning = gpio_state_bucket
+    feedback.construction_bin_raised = gpio_state_bin
+    pub.publish(feedback)
+    rospy.loginfo("bucket state: %s, bin state: %s" % feedback.bucket_spinning, feedback.construction_bin_raised)
 
 if __name__ == "__main__":
     setup_node()
