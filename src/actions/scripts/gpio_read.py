315d970d1817522b255c553957f75d93f69bab67
Mikhail Kornilov <beeboisgr8@gmail.com>
2024-04-07 13:44:23 -0400

    made gpio_read node

---
+++ src/actions/scripts/gpio_read.py
@@ -0,0 +1,18 @@
+import rospy
+from std_msgs.msg import String
+
+pub = None
+def setup_node():
+    global pub
+    pub = rospy.Publisher("gpio", String, queue_size=0)
+    rospy.init_node("gpio_read", anonymous = True)
+
+def publish():
+    global pub
+    #pub.publish("dog")
+
+if __name__ == "__main__":
+    setup_node()
+    while not rospy.is_shutdown():
+        publish()
+    rospy.spin()
