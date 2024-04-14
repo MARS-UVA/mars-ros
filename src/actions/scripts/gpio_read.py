957fa943362554e344079bfb68670770e6a37ed8
Surya Shanmugaselvam <amy6zx@virginia.edu>
2024-04-13 22:09:19 -0400

    Adding limit switch emergency stop capability

--- src/actions/scripts/gpio_read.py
+++ src/actions/scripts/gpio_read.py
@@ -23,7 +23,11 @@ def publish():
     gpio_state_bucket = GPIO.input(channel_bucket)
     gpio_state_bin = GPIO.input(channel_bin)
     feedback.bucket_spinning = gpio_state_bucket
+    if gpio_state_bucket == 1:
+        feedback.bucket_contact_timestamp = rospy.Time.now()
     feedback.construction_bin_raised = gpio_state_bin
+    if gpio_state_bin == 1:
+        feedback.construction_bin_raised_timestamp = rospy.Time.now()
     pub.publish(feedback)
     rospy.loginfo("bucket state: %s, bin state: %s" % (feedback.bucket_spinning, feedback.construction_bin_raised))
 
